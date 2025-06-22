052361bc3bf8e5afdd6bd4312edb28256343c90b
alex <alex_1997_4@hotmail.com>
2025-06-22 10:49:39 -0500

    reportes diaria general y detallado

--- src/app/pages/modules/reportes/reportes.component.ts
+++ src/app/pages/modules/reportes/reportes.component.ts
@@ -1,11 +1,204 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { ImportsModule } from '../../imports';
+import { FormsModule } from '@angular/forms';
+import { PedidoService } from '../../service/pedido.service';
+import { Table } from 'primeng/table';
+import { ES_LOCALE } from '../../../model/util/calendar';
+import { TabsModule } from 'primeng/tabs';
 @Component({
-  selector: 'app-reportes',
-  imports: [],
-  templateUrl: './reportes.component.html',
-  styleUrl: './reportes.component.scss'
+    selector: 'app-reportes',
+    imports: [CommonModule, ImportsModule, FormsModule], // <-- Add this
+    templateUrl: './reportes.component.html',
+    styleUrl: './reportes.component.scss'
 })
 export class ReportesComponent {
+    selectedRange: Date[] = [];
+    selectedRange2: Date | null = null; // Initialize selectedRange2 as null
 
+    esLocale = ES_LOCALE;
+    selectedDates: Date[] = [];
+    @ViewChild('dt') dt!: Table; // Use the Table type from PrimeNGç
+    Clients: any[] = []; // Initialize Clients as an empty array
+    PedidoDetalle: any[] = []; // Initialize PedidoDetalle as an empty array
+    PedidoReporte: any[] = []; // Initialize PedidoReporte as an empty array
+    array_data = [] as any;
+    array_data_total: any[] = []; // Initialize array_data_total as an empty array
+    constructor(private PedidoService: PedidoService) {}
+
+    filterGlobal(event: Event) {
+        const input = event.target as HTMLInputElement; // Type assertion
+        const value = input.value; // Safe access to value
+        this.dt.filterGlobal(value, 'contains'); // Use the dt reference to filter the table
+    }
+
+    handleCalendarBlur() {
+        if (this.selectedRange.length === 2) {
+            if (this.selectedRange[1] != null) {
+                this.showRerporte({
+                    fechainicio: this.formatDateToMySQL(new Date(this.selectedRange[0])),
+                    fechafin: this.formatDateToMySQL(new Date(this.selectedRange[1]))
+                });
+            }
+        } else {
+            console.log('No se ha completado el rango de fechas');
+        }
+    }
+
+    DayCalendarBlur() {
+        debugger;
+        if (this.selectedRange2) {
+            this.showRerporteDay(this.formatDateToMySQL(new Date(this.selectedRange2)));
+        } else {
+            console.log('No se ha completado el rango de fechas');
+        }
+    }
+
+    showRerporte(parameters: any = {}) {
+        this.Clients = [];
+        debugger;
+
+        this.PedidoService.showRerporte(parameters).subscribe(
+            (response: { success: any; data: any[] }) => {
+                if (response.success) {
+                    // response.data.forEach((element: any) => {
+                    //   element.fecha = new Date(element.fecha).toISOString().split('T')[0]; // Formato YYYY-MM-DD
+                    //   element.created_at = this.formatDate(element.created_at);
+                    // });
+                    var yape_total = 0;
+                    var plin_total = 0;
+                    var visa_total = 0;
+                    var efectivo_total = 0;
+                    var yape_total = 0;
+                    var TOTAL_TOTAL = 0;
+                    this.array_data = [] as any;
+                    response.data.forEach((element: any) => {
+                        yape_total += parseInt(element.YAPE);
+                        plin_total += parseInt(element.PLIN);
+                        visa_total += parseInt(element.VISA);
+                        efectivo_total += parseInt(element.EFECTIVO);
+
+                        debugger;
+                        var dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
+                        var numeroDia = new Date(element.fecha).getDay() + 1;
+                        var nombreDia = dias[numeroDia];
+                        var total = parseInt(element.YAPE) + parseInt(element.VISA) + parseInt(element.EFECTIVO) + parseInt(element.PLIN);
+                        TOTAL_TOTAL += total;
+                        if (element.fecha != undefined) {
+                            element.fecha = new Date(element.fecha).toISOString().split('T')[0]; // Formato YYYY-MM-DD
+                            // element.fecha = this.formatDateToMySQL(element.fecha);
+                        } else {
+                            element.fecha = '';
+                        }
+                        this.array_data.push({
+                            fecha: element.fecha,
+                            dia: nombreDia,
+                            yape: element.YAPE,
+                            visa: element.VISA,
+                            efectivo: element.EFECTIVO,
+                            plin: element.PLIN,
+                            total: total
+                        });
+                    });
+                    this.Clients = this.array_data;
+                    const totalEfectivo = this.totalEfectivo;
+                    const totalPlin = this.totalPlin;
+                    const totalVisa = this.totalVisa;
+                    const totalGeneral = this.totalGeneral;
+                    const totalyape = this.totalyape;
+
+                    this.array_data_total.push({
+                        efectivo: totalEfectivo,
+                        plin: totalPlin,
+                        visa: totalVisa,
+                        total: totalGeneral,
+                        yape: totalyape
+                    });
+                } else {
+                    alert('Error al intentar consultar');
+                }
+            },
+            (error: any) => {
+                console.error('Error al intentar consultar', error);
+                alert('Hubo un problema al conectar con el servidor');
+            }
+        );
+    }
+
+    showRerporteDay(parameters: string) {
+        this.Clients = [];
+        debugger;
+
+        this.PedidoService.ShowPedidosFecha(parameters).subscribe(
+            (response: { success: any; data: any[] }) => {
+                if (response.success) {
+                    debugger;
+                    this.array_data = [] as any;
+                    response.data.forEach((element: any) => {
+                        if (element.fecha != undefined) {
+                            element.fecha = new Date(element.fecha).toISOString().split('T')[0]; // Formato YYYY-MM-DD
+                            var dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
+                            var numeroDia = new Date(element.fecha).getDay() + 1;
+                            var nombreDia = dias[numeroDia];
+                            element.dia = nombreDia;
+                        }
+                    });
+                    this.PedidoReporte = response.data;
+                } else {
+                    alert('Error al intentar consultar');
+                }
+            },
+            (error: any) => {
+                console.error('Error al intentar consultar', error);
+                alert('Hubo un problema al conectar con el servidor');
+            }
+        );
+    }
+
+    formatDate(dateString: string | number | Date) {
+        const date = new Date(dateString);
+
+        // Get day, month, year, hours, and minutes
+        const day = String(date.getDate()).padStart(2, '0'); // Add leading zero
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+        const year = date.getFullYear();
+        const hours = String(date.getHours()).padStart(2, '0'); // Add leading zero
+        const minutes = String(date.getMinutes()).padStart(2, '0'); // Add leading zero
+
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+
+        // Usage example
+    }
+    formatDateToMySQL(date: Date): string {
+        debugger;
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+        const day = String(date.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`; // Return the date in YYYY-MM-DD format
+    }
+
+    get totalEfectivo(): number {
+        return this.datosVisibles.reduce((sum, item) => sum + +item.efectivo, 0);
+    }
+
+    get totalyape(): number {
+        return this.datosVisibles.reduce((sum, item) => sum + +item.yape, 0);
+    }
+
+    get totalPlin(): number {
+        return this.datosVisibles.reduce((sum, item) => sum + +item.plin, 0);
+    }
+
+    get totalVisa(): number {
+        return this.datosVisibles.reduce((sum, item) => sum + +item.visa, 0);
+    }
+
+    get totalGeneral(): number {
+        return this.totalEfectivo + this.totalPlin + this.totalVisa + this.totalyape;
+    }
+
+    private get datosVisibles(): any[] {
+        return this.Clients;
+    }
 }
--- src/app/pages/service/pedido.service.ts
+++ src/app/pages/service/pedido.service.ts
@@ -42,6 +42,31 @@ export class PedidoService {
         return this.http.post<any>(this.apiUrl, { query });
     }
 
+    showRerporte(parameters: any = {}): Observable<any> {
+        const query = `select SUM(p.visa) AS VISA,SUM(p.yape) AS YAPE,SUM(p.efectivo) AS EFECTIVO,SUM(p.plin) AS PLIN ,p.fecha
+                  FROM pedido p
+                  WHERE p.estado=3
+                  AND p.fecha BETWEEN '${parameters.fechainicio}' and '${parameters.fechafin}' GROUP BY p.fecha;`;
+        return this.http.post<any>(this.apiUrl, { query });
+    }
+
+    ShowPedidosFecha(parameters: string): Observable<any> {
+        const query = `select
+        p.mesa ,p.idpedido,p.fecha,p.mesa,p.descuento,p.total,p.total_pedidos,  p.yape,  p.plin,  p.efectivo,  p.visa,TIME_FORMAT(p.created_at , '%H:%i') AS hora
+        FROM pedido p
+        WHERE p.estado = 3
+        and p.fecha = '${parameters}' order by p.idpedido  desc`;
+        return this.http.post<any>(this.apiUrl, { query });
+    }
+
+    ReporteProductoDetalle(parameters: string): Observable<any> {
+        const query = `select p1.opcionespedido, p1.pedido_estado, p.descuento,p.comentario, p1.lugarpedido, p1.idproducto,p2.idcategoria, p.idpedido, p1.cantidad,p2.nombre,p1.cantidad,p1.precioU,p1.total,p.mesa,p.total totalidad FROM pedido p
+            INNER JOIN pedidodetalle p1 ON p.idpedido=p1.idpedido
+            INNER JOIN producto p2 ON p1.idproducto=p2.idproducto
+            WHERE  p.fecha='${parameters}' AND p.deleted  IS null  AND p1.deleted  IS null ORDER BY p.mesa;`;
+        return this.http.post<any>(this.apiUrl, { query });
+    }
+
     async EditarPedido(arraypedido: NuevoPedido, comentario: string) {
         const date = new Date();
         const fecha = date.toISOString().split('T')[0];
@@ -156,7 +181,7 @@ export class PedidoService {
     insertPedidoDetalle(arraypedido: NuevoPedidodetalle): Observable<any> {
         var toppings = '';
         if (arraypedido.idtopings.length > 0) {
-            debugger
+            debugger;
             arraypedido.idtopings.forEach((element: any, index: number) => {
                 toppings += `${element.idtopings},`;
             });
