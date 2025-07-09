8c0f9603fed4149d64ec5cffe89315f67a62b68e
alex <alex_1997_4@hotmail.com>
2025-07-09 08:46:51 -0500

    topping crud

--- src/app/pages/modules/configuracion/toppings/toppings.component.ts
+++ src/app/pages/modules/configuracion/toppings/toppings.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ImportsModule } from '../../../imports';
+import { Customer } from '../../../service/customer.service';
 
 @Component({
     selector: 'app-toppings',
-    imports: [],
+    imports: [CommonModule, FormsModule, ImportsModule],
     templateUrl: './toppings.component.html',
     styleUrl: './toppings.component.scss'
 })
-export class ToppingsComponent {}
+export class ToppingsComponent {
+    balanceFrozen: boolean = false;
+    customers2: Customer[] = [];
+
+    formatCurrency(value: number) {
+        return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    }
+}
